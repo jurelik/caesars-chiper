40ca27b14f62c0821bee86294f616a017c0a52e0
jurelik <jure@likar.me>
2018-10-14 11:12:01 +0100

    JS passes FCC challenge

--- cchiper.js
+++ cchiper.js
@@ -1,1 +1,30 @@
-console.log('yo');
+function rot13(str) { // LBH QVQ VG!
+
+  let arr = str.split("");
+  let chiper = [];
+  let highNum = 90;
+  let lowNum = 65;
+  let amountOfDisplacement = 13;
+
+  arr.forEach(char => {
+    let regex = /\W+|_+/;
+
+    if (regex.test(char)) {
+      chiper = chiper.concat(char);
+    }
+    else if (char.charCodeAt() < lowNum + amountOfDisplacement) {
+      chiper = chiper.concat(String.fromCharCode(highNum - (lowNum - (char.charCodeAt() - amountOfDisplacement + 1))));
+    }
+    else {
+      chiper = chiper.concat(String.fromCharCode(char.charCodeAt() - amountOfDisplacement));
+    }
+  });
+
+  str = chiper.join("");
+
+  console.log(str);
+  return str;
+}
+
+// Change the inputs below to test
+rot13("SERR PBQR PNZC");
